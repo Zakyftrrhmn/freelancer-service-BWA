25e270f9e13ddbccfa7bced94e0ce97dcb31da8a
ZakyFathurRahman <zakifaturrahman0@gmail.com>
2024-03-14 20:57:30 +0700

    Custom structure blade templating, Controller & Root

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,8 +10,13 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
-    {
-        return $request->expectsJson() ? null : route('login');
-    }
+    protected function redirectTo($request)
+        {
+           if(! $request->expectsJson()){
+               return route('index');
+           }
+           
+        // redirect tu homepage after login
+        return route('index');
+        }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Front
+use App\Http\Controllers\Landing\LandingController;
+
+// Dashboard
+use App\Http\Controllers\Dashboard\MemberController;
+use App\Http\Controllers\Dashboard\MyOrderController;
+use App\Http\Controllers\Dashboard\ProfileController;
+use App\Http\Controllers\Dashboard\RequestController;
+use App\Http\Controllers\Dashboard\ServiceController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,16 +23,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('detail_booking/{id}', LandingController::class, 'detail_booking')->name('detail.booking.landing');
+Route::get('booking/{id}', LandingController::class, 'booking')->name('booking.landing');
+Route::get('detail/{id}', LandingController::class, 'detail')->name('detail.landing');
+Route::get('explore', LandingController::class, 'explore')->name('explore.landing');
+Route::resource('/', LandingController::class);
+
+
+Route::group(['prefix' => 'member', 'as' => 'member.', 'middleware' => ['auth:sanctum','verified']],
+function(){
+
+    // dashboard
+    Route::resource('dashboard', MemberController::class);
+
+    // Service
+    Route::resource('service', ServiceController::class);
+
+    // Request
+    Route::get('approved_request/{id}', RequestController::class, 'approve')->name('approve.request');
+    Route::resource('request', RequestController::class);
+
+    // MyOrder
+    Route::get('accept/order/{id}', MyOrderController::class, 'accepted')->name('accepted.order');
+    Route::get('reject/order/{id}', MyOrderController::class, 'rejected')->name('rejected.order');
+    Route::resource('order', MyOrderController::class);
+
+    // profile
+    Route::get('delete_photo', ProfileController::class, 'delete')->name('delete.profile');
+    Route::resource('profile', ProfileController::class);
+
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
